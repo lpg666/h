47c61926f45d994d2e899eb5d1494439805e5ec9
lpg666 <1003289700@qq.com>
2017-03-06 16:18:45 +0800

    添加Wechat

--- config/wechat.php
+++ config/wechat.php
@@ -24,7 +24,7 @@ return [
              */
             'log' => [
                 'level' => env('WECHAT_LOG_LEVEL', 'debug'),
-                'file'  => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
+                'file'  => env('WECHAT_LOG_FILE', storage_path('logs\wechat.log')),
             ],
         ],
         'account' => [
