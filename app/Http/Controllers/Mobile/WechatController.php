1f16367a2587abad5575a5ea5f13ab984ad7798a
lpg666 <1003289700@qq.com>
2017-03-21 18:41:00 +0800

    修改

--- app/Http/Controllers/Mobile/WechatController.php
+++ app/Http/Controllers/Mobile/WechatController.php
@@ -17,7 +17,7 @@ class WechatController extends Controller
     public function __construct(){}
 
     public function serve(Request $request){
-        //if($request->input('signature')) return WechatCustom::checkSignature($request);
+        if($request->input('signature')) return WechatCustom::checkSignature($request);
         return WechatCustom::serve();
     }
 
