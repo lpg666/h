c862128f6d2592f15a4fc5a741f5bc63544ea688
lpg <1003289700@qq.com>
2017-04-08 01:42:52 +0800

    wechat

--- resources/views/mobile/yourWechatData.blade.php
+++ resources/views/mobile/yourWechatData.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ elixir('mobile/css/app.css') }}">
     <link rel="stylesheet" href="/mobile/css/webuploader.css">
-    <title>1</title>
+    <title>12</title>
 </head>
 
 <body>
@@ -20,7 +20,7 @@
     var BASE_URL = '/mobile';
 </script>
 <script src="/mobile/js/jquery-1.8.3.min.js"></script>
-<script src="/mobile/js/webuploader.min.js"></script>
+<script src="/mobile/js/webuploader.js"></script>
 <script src="/mobile/js/getting-started.js"></script>
 
 </body>
