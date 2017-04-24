f01411403937cabaad1c32221fc647715f452262
lpg666 <1003289700@qq.com>
2017-04-24 15:03:10 +0800

    修改

--- app/Services/functions.php
+++ app/Services/functions.php
@@ -129,7 +129,6 @@ function saveUrlImage($url, $save_path) {
 function uploadUpyun($localFile, $upyunFile)
 {
     $upyun = new UpYun(config('app.upyun_bucketname'), config('app.upyun_operator_name'), config('app.upyun_operator_pwd'));
-    dd($upyun);
     try {
         $fh = @fopen($localFile, 'rb');
         $rsp = @$upyun->writeFile($upyunFile, $fh, true);   // 上传图片，自动创建目录
@@ -137,6 +136,7 @@ function uploadUpyun($localFile, $upyunFile)
         $file = config('app.upyun_domain') . $upyunFile;
     } catch (Exception $e) {
         $file = trim($localFile, '.');
+        dd($e);
     }
     return $file;
 }
