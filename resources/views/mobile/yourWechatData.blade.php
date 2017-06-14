e5d0eb0cdf37e9c08b9e972a7562c66b7d7cf908
lpg666 <1003289700@qq.com>
2017-06-14 09:59:10 +0800

    修改

--- resources/views/mobile/yourWechatData.blade.php
+++ resources/views/mobile/yourWechatData.blade.php
@@ -8,66 +8,22 @@
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     <script type="text/javascript" src="{{asset('mobile/js/jquery-1.8.3.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('mobile/js/fastclick.js')}}"></script>
-    <script type="text/javascript" src="{{asset('mobile/js/canvas2d.js')}}"></script>
-    <script type="text/javascript" src="{{asset('mobile/js/GuaGuaLe2.js')}}"></script>
     <style>
-        body
-        {
-            background: url("s_bd.jpg") repeat 0 0;
-        }
-        .container
-        {
-            position: relative;
-            width: 400px;
-            height: 160px;
-            margin: 100px auto 0;
-            background: url(s_title.png) no-repeat 0 0;
-            background-size: 100% 100%;
-        }
-        #front, #back
-        {
-            position: absolute;
-            width: 200px;
-            left: 50%;
-            top: 100%;
-            margin-left: -130px;
-            height: 80px;
-            border-radius: 5px;
-            border: 1px solid #444;
-        }
+
     </style>
-    <script>
-        wx.config({!! \App\Services\WechatCustom::jssdk('service') !!});
-        var share_info = {
-            title: "a",
-            desc: "b",
-            imgUrl: "",
-            link: "{{request()->fullUrl()}}"
-        };
-        var timeline_share_info = {
-            title: "a",
-            desc: "b",
-            imgUrl: "",
-            link: "{{request()->fullUrl()}}"
-        };
-        wx.ready(function(){
-            wx.onMenuShareWeibo(share_info);
-            wx.onMenuShareAppMessage(share_info);
-            wx.onMenuShareQQ(share_info);
-            wx.onMenuShareTimeline(timeline_share_info);
-        });
-    </script>
 </head>
 
 <body>
-    <div class="container">
-        <canvas id="back" width="200" height="80"></canvas>
-        <canvas id="front" width="200" height="80"></canvas>
-    </div>
+    <div id="comment_but">aaaaaa</div>
+    <input autofocus="true" class="pin_text">
 </body>
 </html>
 <script>
     FastClick.attach(document.body);
-    var guaguale = new GuaGuaLe("front", "back");
-    guaguale.init({msg: "￥5000000.00"});
+    var button = document.getElementById('comment_but');
+    var inputElem = document.getElementsByClassName('pin_text')[0];
+    button.addEventListener('click', function(){
+        console.log(inputElem);
+        inputElem.focus();
+    });
 </script>
