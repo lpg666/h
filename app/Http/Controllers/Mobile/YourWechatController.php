7a07027d5c72ef83ed83bc61c94eeb2276eb6b58
lpg666 <1003289700@qq.com>
2017-07-17 18:36:18 +0800

    修改

--- app/Http/Controllers/Mobile/YourWechatController.php
+++ app/Http/Controllers/Mobile/YourWechatController.php
@@ -28,37 +28,6 @@ class YourWechatController extends Controller
         }
     }
 
-    public function anyFx(Request $request){
-        $curl = curl_init();
-        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx45a224e60d651136&secret=d7a975ce04146ce217d721e60c56b2af';
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $data = curl_exec($curl);
-        curl_close($curl);
-        $data=json_decode($data);
-        $access_token=$data->access_token;
-
-        $curl = curl_init();
-        $url='https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$access_token.'&type=jsapi';
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $data = curl_exec($curl);
-        curl_close($curl);
-        $data=json_decode($data);
-        $ticket=$data->ticket;
-
-        $noncestr = 'weixin';
-        $timestamp = time();
-        $url = $request->input('url');
-        $jsapi_ticket = sha1('jsapi_ticket='.$ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url);
-
-        $data->appid = 'wx45a224e60d651136';
-        $data->timestamp = $timestamp;
-        $data->noncestr = $noncestr;
-        $data->signature = $jsapi_ticket;
-        return json_encode($data);
-    }
-
     private function _loginSession($request)
     {
         if($request->input('subscribe') !== '0'){
@@ -67,5 +36,122 @@ class YourWechatController extends Controller
             if ($member instanceof RedirectResponse) return $member;
         }
         return '';
+    }
+
+    public function anyFx(Request $request){
+        $jsapiTicket = $this->getJsApiTicket();
+
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $noncestr = $this->createNonceStr();
+        $timestamp = time();
+
+        $string ='jsapi_ticket='.$jsapiTicket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url;
+
+        $jsapi_ticket = sha1($string);
+
+        $signPackage = array(
+            "appId"     => 'wx45a224e60d651136',
+            "nonceStr"  => $noncestr,
+            "timestamp" => $timestamp,
+            "url"       => $url,
+            "signature" => $jsapi_ticket,
+            "rawString" => $string
+        );
+
+        return json_encode($signPackage);
+    }
+
+    private function createNonceStr($length = 16) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    private function getJsApiTicket() {
+        $data=session('jsapi_ticket');
+        if($data){
+            if ($data->expire_time < time()) {
+                $accessToken = $this->getAccessToken();
+
+                $curl = curl_init();
+                $url='https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$accessToken.'&type=jsapi';
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                $datas = curl_exec($curl);
+                curl_close($curl);
+                $datas=json_decode($datas);
+                $ticket=$datas->ticket;
+                if($ticket){
+                    $data['expire_time'] = time() + 7000;
+                    $data['jsapi_ticket'] = $ticket;
+                    session('jsapi_ticket',$data);
+                }
+            }else{
+                $ticket = $data->jsapi_ticket;
+            }
+        }else{
+            $accessToken = $this->getAccessToken();
+            $curl = curl_init();
+            $url='https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$accessToken.'&type=jsapi';
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $datas = curl_exec($curl);
+            curl_close($curl);
+            $datas=json_decode($datas);
+            $ticket=$datas->ticket;
+            if($ticket){
+                $data['expire_time'] = time() + 7000;
+                $data['jsapi_ticket'] = $ticket;
+                session('jsapi_ticket',$data);
+            }
+        }
+
+        return $ticket;
+    }
+
+    private function getAccessToken() {
+
+        $data=session('access_token');
+        if($data){
+            if ($data->expire_time < time()) {
+                $curl = curl_init();
+                $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx45a224e60d651136&secret=d7a975ce04146ce217d721e60c56b2af';
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                $datas = curl_exec($curl);
+                curl_close($curl);
+                $datas=json_decode($datas);
+                $access_token=$datas->access_token;
+                if ($access_token) {
+                    $data['expire_time'] = time() + 7000;
+                    $data['access_token'] = $access_token;
+                    session('access_token',$data);
+                }
+            }else{
+                $access_token = $data->access_token;
+            }
+        }else{
+            $curl = curl_init();
+            $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx45a224e60d651136&secret=d7a975ce04146ce217d721e60c56b2af';
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $datas = curl_exec($curl);
+            curl_close($curl);
+            $datas=json_decode($datas);
+            $access_token=$datas->access_token;
+            if ($access_token) {
+                $data['expire_time'] = time() + 7000;
+                $data['access_token'] = $access_token;
+                session('access_token',$data);
+            }
+        }
+
+        return $access_token;
+
     }
 }
