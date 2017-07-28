04641855fe35393eed99ef81947b728a463396c7
lpg <1003289700@qq.com>
2017-07-29 00:44:53 +0800

    修改

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,10 +7,10 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'http://vue.xfb315.com');
+        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Credentials', 'false');
         return $response;
     }
 }
