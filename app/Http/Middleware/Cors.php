14d5883f392fa68ba623afc45a389947e88564f2
lpg <1003289700@qq.com>
2017-07-28 23:35:51 +0800

    修改

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,16 +1,14 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
-use Response;
 class Cors
 {   //跨域中间建
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'vue.xfb315.com');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        $response->header('Access-Control-Allow-Credentials', 'true');
-        return $response;
+        $request->header('Access-Control-Allow-Origin', '*');
+        $request->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
+        $request->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $request->header('Access-Control-Allow-Credentials', 'false');
+        return  $next($request);
     }
 }
