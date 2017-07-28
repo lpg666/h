1f102b0d63849b83699f26bc059358ab475f9cdb
lpg <1003289700@qq.com>
2017-07-29 00:58:58 +0800

    修改

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,10 +7,10 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', 'http://m.s1.lpg6.xyz');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        $response->header('Access-Control-Allow-Credentials', 'false');
+        $response->header('Access-Control-Allow-Credentials', 'true');
         return $response;
     }
 }
