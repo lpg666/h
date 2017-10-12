fdc637d241547404219882c4115b9552950e7ef2
lpg <1003289700@qq.com>
2017-10-12 14:00:34 +0800

    修改

--- app/Http/Controllers/Api/v1/GmAutoController.php
+++ app/Http/Controllers/Api/v1/GmAutoController.php
@@ -39,6 +39,7 @@ class GmAutoController extends ApiController{
                 'enterprise_id' => 0,
                 'online_time' => 0
             ]);
+            $data = (object);
             $data->email = $operator->email;
             $data->last_ip = $operator->last_ip;
             $data->name = $operator->name;
